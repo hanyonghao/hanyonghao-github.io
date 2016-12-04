5cbe7643cc1f6174316388c88c2aa9bb1c10dd79
永豪 <lo0o0ol@yeah.net>
2016-12-04 10:49:20 +0800

    数据测试

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 (function(){
-    var host = "http://" + window.location.host;
+    var host = window.location.origin;
     host = host.indexOf("localhost") > -1 ?  host + "/hanyonghao.github.io" : host;
     $.getJSON(host + "/data/test.json",function(result){
         console.log(result)
