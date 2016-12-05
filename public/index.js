d2fa791ffd42a3e87d9bd22d5181c32669034b3d
永豪 <lo0o0ol@yeah.net>
2016-12-05 19:27:49 +0800

    数据测试

--- public/index.js
+++ public/index.js
@@ -1,7 +1,15 @@
-(function(){
-    var host = window.location.origin;
-    host = host.indexOf("localhost") > -1 ?  host + "/hanyonghao.github.io" : host;
-    $.getJSON(host + "/data/test.json",function(result){
-        console.log(result)
+(function($){
+
+    $(document).ready(function(){
+        $.getJSON(getRoot() + "/data/test.json",function(result){
+            console.log(result)
+        });
     });
-})();
+
+    function getRoot(){
+        var host = window.location.origin;
+        host = host.indexOf("localhost") > -1 ?  host + "/hanyonghao.github.io" : host;
+        return host;
+    }
+
+})(jQuery);
